e4835fd333b6d655deb40c09dcaa5f17eb3b8b93
Amid Sanai <sanaiamid@gmail.com>
2023-11-26 20:37:43 +0330

    added a readme file; modified the checking mechanism
    the for loop breaks after first encounter with a non palindrome case.

--- Palindrome.java
+++ Palindrome.java
@@ -5,8 +5,10 @@ public class Palindrome {
 		String string = input.next();
 		boolean bool = true;
 		for(int i = 0; i < string.length(); i++)
-			if (!(String.valueOf(string.charAt(i)).equals(String.valueOf(string.charAt(string.length()-i-1))))) 
+			if (!(String.valueOf(string.charAt(i)).equals(String.valueOf(string.charAt(string.length()-i-1))))) {
 				bool = false;
+				break;
+			}
 		System.out.printf("%s is %sa palindrome.%n", string, (bool ? "" : "not "));
 	}
 }
---
+++ README.md
@@ -0,0 +1,2 @@
+A program that checks if a string is a palindrome of not.
+not sure about the mechanism that checks the string.

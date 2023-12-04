f96ef719b7391bbdaa0f5a971d1e4bcfaad66499
Amid Sanai <sanaiamid@gmail.com>
2023-12-04 21:36:54 +0330

    updated if condition
    optimized the futile checking of chars after the string's length devided by two.

--- Palindrome.java
+++ Palindrome.java
@@ -4,7 +4,7 @@ public class Palindrome {
 		Scanner input = new Scanner(System.in);
 		String string = input.nextLine();
 		boolean bool = true;
-		for(int i = 0; i < string.length(); i++)
+		for(int i = 0; i < string.length()/2; i++)
 			if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
 				bool = false;
 				break;

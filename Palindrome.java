6335c4573623549f81d6fbfe3b8b7167e2ff5565
Amid Sanai <sanaiamid@gmail.com>
2023-12-07 12:07:56 +0330

    changed the mechanism back to the for loop
    apparently for loop iteration is faster than recursion

--- Palindrome.java
+++ Palindrome.java
@@ -3,14 +3,12 @@ public class Palindrome {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		String string = input.nextLine();
-		System.out.printf("%s is %sa palindrome.%n", string, (isPalindrome(string.toCharArray(), 0) ? "" : "not "));
-	}
-
-	private static boolean isPalindrome(char[] chars, int i) {
-		if (i == chars.length/2)
-			return true;
-		if (chars[i] == chars[chars.length - 1 - i])
-			return isPalindrome(chars, ++i);
-		return false;
+		boolean isPalindrome = true;
+		for(int i = 0; i < string.length()/2; i++)
+			if (string.charAt(i) != string.charAt(string.length() - i - 1)) {
+				isPalindrome = false;
+				break;
+			}
+		System.out.printf("%s is %sa palindrome.%n", string, isPalindrome ? "" : "not ");
 	}
 }
